89e09136e1a4d7537e82a0bedc2fd5c6b42b29ee
Michael Jefferys <michael@michaeljefferys.com>
2018-06-15 12:38:06 +0100

    MJ - Add functions to change speed of playback using f keys
    MJ - Add functions to change speed of playback using f keys

--- renderer.js
+++ renderer.js
@@ -6,14 +6,29 @@ document.addEventListener("keydown", function (e) {
     var appVersion = require('electron').remote.app.getVersion();
     var electrionVersion = process.versions.electron;
     //var electrionVersion = 'hello';
-    if (e.which === 114) {
-        require('remote').getCurrentWindow().openDevTools();
+    if (e.which === 123) {
+        require('remote').getCurrentWindow().toggleDevTools();
     } else if (e.which === 116) {
         location.reload();
     } else if (e.which === 112) {
         alert('Version: ' + appVersion + ' Electron version: ' + electrionVersion);    
     } else if (e.which == 113){
-        alert('Version: ' + appVersion + ' Electron version: ' + electrionVersion);    
-        //remote.getCurrentWindow().maximize();    
+        changeSpeed(1)
+    } else if (e.which == 114){
+        changeSpeed(1.25)  
+    } else if (e.which == 115){
+        changeSpeed(1.5)
+    } else if (e.which == 117){
+        changeSpeed(2)
     }
 });
+
+function changeSpeed(speed)
+{   
+    console.log('Setting speed to: ' + speed)
+    const webview = document.querySelector('webview');             
+    var speedJavaScript = "var videos = document.querySelectorAll('video'); videos.forEach(function(video) { video.playbackRate=" + speed + "; });"
+    //console.log(jacascript);
+    webview.executeJavaScript(speedJavaScript);
+    console.log('Speed to: ' + speed)
+}
